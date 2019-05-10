d4340aecbbc649ef94efe6ec538f63f2ae1e1785
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-05-10 07:37:54 -0400

    Add and array for computer to generate a chakra from

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,20 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int number;
+        number =(int)(Math.random()*5);
+
+        System.out.println(number);
+
+        String [] chakra = new String[5];
+            chakra[0] = "fire";
+            chakra[1] = "wind";
+            chakra[2] = "lightning";
+            chakra[3] = "earth";
+            chakra[4] = "water";
+
+        System.out.println( chakra[number]);
+
     }
+
 }
