7a061f6ecb0fb4247ce2b7582876d45aae82711c
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-05-16 08:21:33 -0400

    added scoring system

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,12 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-        int number;
-        String shinobi;
+        int cpuPoints,user,cpu,userPoints;
 
-        number =(int)(Math.random()*5);
+        cpuPoints = 0;
+        userPoints = 0;
+        String cpuChakra,userChakra;
 
-        System.out.println(number);
+
+
 
         String [] chakra = new String[5];
             chakra[0] = "fire";
@@ -19,10 +21,56 @@ public class Main {
             chakra[3] = "earth";
             chakra[4] = "water";
 
-        System.out.println( chakra[number]);
 
-        shinobi= keyboard.nextLine();
 
+        System.out.println("Pick a Chakra Nature");
+
+        while(true) {
+            while (true) {
+
+                cpu =(int)(Math.random()*5);
+                cpuChakra = chakra[cpu];
+                System.out.println(cpuChakra);
+
+                user = keyboard.nextInt();
+                userChakra = chakra[user];
+                System.out.println(userChakra);
+
+                switch (cpu) {
+                    case 0:
+                        if (userChakra == "wind" || userChakra == "lightning") {
+                            cpuPoints++;
+                        }
+                        break;
+                    case 1:
+                        if (userChakra == "lightning" || userChakra == "earth") {
+                            cpuPoints++;
+
+                        }
+                    case 2:
+
+                        if (userChakra == "earth" || userChakra == "water") {
+                            cpuPoints++;
+
+                        }
+                        break;
+                    case 3:
+
+                        if (userChakra == "water" || userChakra == "fire") {
+                            cpuPoints++;
+
+                        }
+                        break;
+                    case 4:
+                        if (userChakra == "fire" || userChakra == "wind") {
+                            cpuPoints++;
+                        }
+                        break;
+
+                }
+                System.out.println("cpu points"+cpuPoints+"my points"+userPoints);
+            }
+        }
 
     }
 
